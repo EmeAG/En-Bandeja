3de1db14ef05674f1f4eaa841a278c9b9d461052
dani020998 <43583536+dani020998@users.noreply.github.com>
2019-11-27 17:25:19 +0100

    movimiento camarero y desequilibrio de vasos por este movimiento

---
+++ onPlate/Assets/Scripts/Waiter.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waiter : MonoBehaviour
+{
+    public float speedMove, speedDash, forceMove, forceDash;
+    Vector3 pos;
+    float clickedA, clickedD, time;
+    float timeDelay = 0.3f;
+    bool dash;
+    Rigidbody[] vasos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        vasos= GetComponentsInChildren<Rigidbody>();
+        dash = false;
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkDoublePress();
+        gestionMovement();
+    }      
+    
+    void gestionMovement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (dash)
+            {
+                pos.x += speedDash*Time.deltaTime;
+                foreach (Rigidbody r in vasos)
+                {
+                    r.AddForce(new Vector3(-forceDash, 0, 0));
+                }
+            }
+            else
+            {
+                pos.x += speedMove*Time.deltaTime;
+                foreach(Rigidbody r in vasos)
+                {
+                    r.AddForce(new Vector3(-forceMove, 0, 0));
+                }
+            }
+            transform.position = pos;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            if (dash)
+            {
+                clickedA = 0;
+                dash = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (dash)
+            {
+                pos.x -= speedDash * Time.deltaTime;
+                foreach (Rigidbody r in vasos)
+                {
+                    r.AddForce(new Vector3(forceDash, 0, 0));
+                }
+            }
+            else
+            {
+                pos.x -= speedMove * Time.deltaTime;
+                foreach (Rigidbody r in vasos)
+                {
+                    r.AddForce(new Vector3(forceMove, 0, 0));
+                }
+            }
+            transform.position = pos;
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            if (dash)
+            {
+                clickedD = 0;
+                dash = false;
+            }
+        }
+    }
+
+    void checkDoublePress()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            clickedA++;
+            if (clickedA == 1)
+            {
+                time = Time.time;
+            }
+        }
+        if (!dash)
+        {
+            if (clickedA > 1 && Time.time - time < timeDelay)
+                {
+                    clickedA = 0;
+                    time = 0;
+                    dash = true;
+                }
+            else if (Time.time - time > timeDelay) clickedA = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            clickedD++;
+            if (clickedD == 1)
+            {
+                time = Time.time;
+            }
+        }
+        if (!dash)
+        {
+            if (clickedD > 1 && Time.time - time < timeDelay)
+            {
+                clickedD = 0;
+                time = 0;
+                dash = true;
+            }
+            else if (Time.time - time > timeDelay) clickedD = 0;
+        }
+    }
+}
